de8a9bf06fec0b14a7047c56bcdb178e7bc5c563
cmseric <512315211@qq.com>
2018-12-24 15:55:25 +0800

    init

---
+++ test.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal(-1, [1, 2, 3].indexOf(-1))
+    })
+  })
+})
+
+describe('测试套件描述', function() {
+  it('测试用例描述: 1 + 2 = 3', function() {
+    // 测试代码
+    const result = 1 + 2;
+    // 测试断言
+    assert.equal(result, 3);
+  });
+});
